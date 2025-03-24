e951e3d68b80542904f5a006b73dff00fbd9bc08
mikaelim <mikaelim.id@gmail.com>
2025-03-24 08:57:39 +0800

    fix: update tile provider

--- lib/src/location_picker.dart
+++ lib/src/location_picker.dart
@@ -2,10 +2,8 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart'
     as marker;
 import 'package:http/http.dart' as http;
